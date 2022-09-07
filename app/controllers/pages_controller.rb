8aaa4f21f6d294a2b3b6cab7254c4007b5dfa35d
ajmal rambocus <ajmalrambocus@gmail.com>
2022-09-07 14:31:17 +0400

    fixed controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @stations = Station.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @stations.geocoded.map do |station|
+      {
+        lat: station.latitude,
+        lng: station.longitude
+      }
+    end
   end
 end
