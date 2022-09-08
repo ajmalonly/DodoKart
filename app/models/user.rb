c8fe4af4cd432647d4df401915d2f5cc23c84e38
ajmal rambocus <ajmalrambocus@gmail.com>
2022-09-08 10:59:31 +0400

    controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_many :trips
   has_many :tickets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
