eb1253c4822361158aa5eb9532ec4dff6928915b
ajmal rambocus <ajmalrambocus@gmail.com>
2022-09-13 13:50:05 +0400

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,26 @@ class PagesController < ApplicationController
   end
 
   def itineraries
+    @stations = Station.all
+    @station_options = @stations.map{ |s| [ s.name ] }
     if params[:origin].present? && params[:destination].present?
-      @itineraries = Itinerary.where("origin ILIKE ? AND destination ILIKE ?", "%#{params[:origin]}%", "%#{params[:destination]}%")
-    elsif params[:orgin].present?
-      @itineraries = Itinerary.where("origin ILIKE?", "%#{params[:origin]}%")
-    elsif params[:destination].present?
-      @itineraries = Itinerary.where("destination ILIKE ?", "%#{params[:destinaton]}%")
-    else
-      @itineraries = Itinerary.all
+      if params[:origin] == params[:destination]
+        @i = Itinerary.new
+        @i.errors.add :origin, "Can't be the same as destination"
+        render "itineraries/index", status: :unprocessable_entity
+      else
+       @itineraries = Itinerary.where("origin ILIKE ? AND destination ILIKE ?", "%#{params[:origin]}%", "%#{params[:destination]}%")
     end
 
-
+  #   elsif params[:orgin].present?
+  #     @itineraries = Itinerary.where("origin ILIKE?", "%#{params[:origin]}%")
+  #   elsif params[:destination].present?
+  #     @itineraries = Itinerary.where("destination ILIKE ?", "%#{params[:destinaton]}%")
+  #   #   @itineraries.each do |i|
+  #   #   end
+  #   #   render "itinaries", status: :unprocessable_entity
+  #   else
+      # @itineraries = Itinerary.all
+    end
   end
 end
